0a2c60c94fd36e195dcc57e6b502cce78cb7ff8c
Analyzer <analyzer@test.com>
2026-02-09 13:04:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,12 @@
-def main():
-    import sys
-    input = sys.stdin.read().splitlines()
-    n, r, c = map(int, input[0].split())
-    words = input[1].split()
-    if not words:
-        print()
-        return
-
-    # Precompute prefix sums and s array
-    prefix_sum = [0] * (n + 1)
-    for i in range(n):
-        prefix_sum[i + 1] = prefix_sum[i] + len(words[i])
-    s = [prefix_sum[i] + i for i in range(n + 1)]
-
-    # Compute next_line_start
-    next_line_start = [0] * n
-    for l in range(n):
-        target = s[l] + 
+n, a, b, c = map(int, input().split())
+total = 0
+max_z = min(c, (2 * n) // 4)
+for z in range(max_z + 1):
+    rem = 2 * n - 4 * z
+    delta = rem - a
+    y_min = max(0, (delta + 1) // 2)
+    y_max = min(b, rem // 2)
+    if y_min > y_max:
+        continue
+    total += y_max - y_min + 1
+print(total)
