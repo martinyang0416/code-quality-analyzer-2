9fa9a1b44dbf44dfc330712706d357f3f31c8f9b
Analyzer <analyzer@test.com>
2026-02-09 14:17:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,14 @@
-def isMatch(s: str, p: str) -> bool:
-    m, n = len(s), len(p)
-    dp_prev = [False] * (n + 1)
-    dp_prev[0] = True  # Empty pattern matches empty string
-
-    # Initialize for the case when the string is empty
-    for j in range(1, n + 1):
-        if p[j - 1] == '*' and dp_prev[j - 1]:
-            dp_prev[j] = True
-        else:
-            break  # Once a non '*' is found, subsequent cannot be matched
-
-    # Iterate over each character in the string
-    for i in range(1, m + 1):
-        dp_cur
+def isSubsequence(s: str, t: str) -> bool:
+    ptr = 0
+    len_t = len(t)
+    for char in s:
+        found = False
+        while ptr < len_t:
+            if t[ptr] == char:
+                ptr += 1
+                found = True
+                break
+            ptr += 1
+        if not found:
+            return False
+    return True
