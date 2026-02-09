eefa8d865a82d3170e2660cffc733db5c637787d
Analyzer <analyzer@test.com>
2026-02-09 10:05:09 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,17 @@
-n, k = map(int, input().split())
+n = int(input())
+t = input().strip()
 
-max_m = 0
-low = 0
-high = k
+s = []
+index = 0
+group_size = 1
 
-while low <= high:
-    mid = (low + high) // 2
-    t = n - 1  # number of terms to sum
-    s = 0
-    current = mid // 2
-    steps = t
-    while steps > 0 and current > 0:
-        s += current
-        current = current // 2
-        steps -= 1
-    total = mid + s
-    if total <= k:
-        max_m = mid
-        low = mid + 1
-    else:
-        high = mid - 1
+while index < len(t):
+    # Extract the group of current group_size
+    group = t[index:index + group_size]
+    # Append the first character of the group to s
+    s.append(group[0])
+    # Move to the next group
+    index += group_size
+    group_size += 1
 
-print(max_m)
+print(''.join(s))
