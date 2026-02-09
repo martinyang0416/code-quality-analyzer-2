178a9ffd030d6341a691add4d51310f82e221316
Analyzer <analyzer@test.com>
2026-02-09 11:35:07 -0800

    Update

--- code.py
+++ code.py
@@ -6,20 +6,20 @@ def main():
     idx = 0
     N = int(input[idx])
     idx += 1
-    A = list(map(int, input[idx:idx+N]))
+    a = list(map(int, input[idx:idx+N]))
     idx += N
-    S = sorted(A)
-    P = [0] * (N + 1)
+    S = sorted(a)
+    prefix = [0] * (N + 1)
     for i in range(N):
-        P[i+1] = P[i] + S[i]
+        prefix[i+1] = prefix[i] + S[i]
+    
+    # Compute initial T
     T = 0
     for i in range(N):
-        T += S[i] * (i + 1)
+        T += (i + 1) * S[i]
+    
     Q = int(input[idx])
     idx += 1
     for _ in range(Q):
         i = int(input[idx])
-        j = int(input[idx+1])
-        idx += 2
-        x = A[i-1]
-        pos
+        j = int(input[idx+1]
