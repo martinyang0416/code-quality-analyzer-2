c53ca1797251622a23351043e521e160c08156d1
Analyzer <analyzer@test.com>
2026-02-09 11:13:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,3 @@
-n, m = map(int, input().split())
-m_squared = m * m
-if n < m_squared and n != m_squared - 1:
-    print("YES")
-else:
-    print("NO")
+a, b = map(int, input().split())
+reversed_b = int(str(b)[::-1])
+print(a + reversed_b)
