9f15e5ba0d86344b0db0cb28672ccaaf9ad1cd4f
Analyzer <analyzer@test.com>
2026-02-09 14:02:43 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,22 @@
-a, b, h, w, n = map(int, input().split())
-a_list = list(map(int, input().split()))
+n = int(input())
+s = input().strip()
+mod = 10**18 + 3
+base = 911382629
 
-def compute_min_k(a_target, b_target, h_initial, w_initial, sorted_a):
-    # Compute product needed
-    if h_initial >= a_target:
-        ph = 1
-    else:
-        ph = (a_target + h_initial - 1) // h_initial
-    if w_initial >= b_target:
-        pw = 1
-    else:
-        pw = (b_target + w_initial - 1) // w_initial
-    # Compute prefix up to min(n, 40)
-    max_k = min(len(sorted_a), 40)
-    prefix = [1]
-    for ai
+prefix_hash = [0] * (n + 1)
+power = [1] * (n + 1)
+for i in range(n):
+    prefix_hash[i+1] = (prefix_hash[i] * base + ord(s[i])) % mod
+    power[i+1] = (power[i] * base) % mod
+
+def get_hash(l, r):
+    if l > r:
+        return 0
+    res = (prefix_hash[r+1] - prefix_hash[l] * power[r - l + 1]) % mod
+    return res if res >= 0 else res + mod
+
+result = []
+max_k = (n + 1) // 2
+for k in range(1, max_k + 1):
+    i = k - 1
+    j = n 
