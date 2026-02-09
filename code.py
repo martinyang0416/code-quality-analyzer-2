ad8329b00d5a3037a42846284871397acc259a79
Analyzer <analyzer@test.com>
2026-02-09 13:24:08 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,15 @@
-Q = int(input())
-for _ in range(Q):
-    n = int(input())
-    seen = set()
-    steps = 0
-    current = n
-    while True:
-        if current < 10:
-            print(steps)
-            break
-        if current in seen:
-            print(-1)
-            break
-        seen.add(current)
-        s = str(current)
-        max_prod = 0
-        for i in range(1, len(s)):
-            left = int(s[:i])
-            right = int(s[i:])
-            prod = left * right
-            if prod > max_prod:
-            
+n = int(input())
+A = list(map(int, input().split()))
+swap_count = 0
+
+for i in range(n):
+    mini = i
+    for j in range(i, n):
+        if A[j] < A[mini]:
+            mini = j
+    if i != mini:
+        A[i], A[mini] = A[mini], A[i]
+        swap_count += 1
+
+print(' '.join(map(str, A)))
+print(swap_count)
