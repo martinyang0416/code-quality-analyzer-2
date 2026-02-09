ce69920a6344ae7aeb397cc2c0e9f8e2e9ba4542
Analyzer <analyzer@test.com>
2026-02-09 10:41:49 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,11 @@
-import sys
-
-def readints():
-    return list(map(int, sys.stdin.readline().split()))
-
-def are_colinear_and_share_endpoint(seg1, seg2):
-    points1 = seg1
-    points2 = seg2
-
-    common = None
-    for p1 in points1:
-        for p2 in points2:
-            if p1 == p2:
-                common = p1
-                break
-        if common is not None:
-            break
-
-    if common is None:
-        return None  # No common endpoint
-
-    # Now, check if other points are colinear
-    # Get the other po
+p = int(input())
+vowels = {'a', 'e', 'i', 'o', 'u'}
+for _ in range(p):
+    s1 = input().strip()
+    s2 = input().strip()
+    s1_vowels = set(c for c in s1 if c in vowels)
+    s2_vowels = set(c for c in s2 if c in vowels)
+    if s1_vowels & s2_vowels:
+        print("YES")
+    else:
+        print("NO")
