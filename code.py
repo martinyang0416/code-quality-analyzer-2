1e15c5eab29130b97caeb3c984b3bca095e17a4b
Analyzer <analyzer@test.com>
2026-02-09 10:35:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,29 @@
-import sys
-import bisect
+def compute_h(x):
+    res = 0
+    while x > 0:
+        res += x
+        x = x // 2
+    return res
 
-class DSU:
-    def __init__(self, n):
-        self.parent = list(range(n))
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        fx = self.find(x)
-        fy = self.find(y)
-        if fx != fy:
-            self.parent[fy] = fx
+def compute_j(x):
+    if x == 1:
+        return 1
+    if x % 2 == 1:
+        return x
+    m = x
+    k = 0
+    while m % 2 == 0:
+        m = m // 2
+        k += 1
+    return (m << k) + ((1 << k) - 1)
 
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    idx = 0
-    t = int(data[idx])
-    idx += 1
- 
+def calc(x):
+    return compute_h(x) + compute_j(x)
+
+def solve():
+    import sys
+    input = sys.stdin.read().split()
+    T = int(input[0])
+    idx = 1
+    for _ in range(T):
+        P = int(input[idx]
