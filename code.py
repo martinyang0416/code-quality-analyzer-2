9e18ccb58e84069eda2110494df38fc736125d9c
Analyzer <analyzer@test.com>
2026-02-09 13:59:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,7 @@
 t = int(input())
 for _ in range(t):
-    n, k, d = map(int, input().split())
-    a = list(map(int, input().split()))
-    min_subs = float('inf')
-    for i in range(n - d + 1):
-        window = a[i:i+d]
-        current = len(set(window))
-        if current < min_subs:
-            min_subs = current
-    print(min_subs)
+    s = input().strip()
+    cnt0 = s.count('0')
+    cnt1 = s.count('1')
+    min_cnt = min(cnt0, cnt1)
+    print("DA" if min_cnt % 2 == 1 else "NET")
