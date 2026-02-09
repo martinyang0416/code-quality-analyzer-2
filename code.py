ddc6f20b7527210ee8948fd78d5635648955d3c3
Analyzer <analyzer@test.com>
2026-02-09 11:25:43 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,21 @@
 import bisect
 
 def putaway(A, B, T, X, Y, W, S):
-    sorted_X = sorted(X)
-    sorted_Y = sorted(Y)
-    
-    W_only = 0
-    S_only = 0
-    Both = 0
-    
+    can_weak = [False] * T
+    can_small = [False] * T
     for i in range(T):
         w = W[i]
         s = S[i]
-        # Check eligibility for weak robots
-        idx_weak = bisect.bisect_right(sorted_X, w)
-        is_weak = (idx_weak < len(sorted_X))
-        # Check eligibility for small robots
-        idx_small = bisect.bisect_right(sorted_Y, s)
-        is_small = (idx_small < len(sorted_Y))
-     
+        possible = any(x > w for x in X)
+        can_weak[i] = possible
+        possible_small = any(y > s for y in Y)
+        can_small[i] = possible_small
+        if not can_weak[i] and not can_small[i]:
+            return -1
+
+    M_list = []
+    O_list = []
+    P_list = []
+    for i in range(T):
+        cw = can_weak[i]
+        cs = c
