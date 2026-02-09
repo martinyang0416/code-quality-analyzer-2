639aac474aa070cd038f858172d5d931ab4c7117
Analyzer <analyzer@test.com>
2026-02-09 13:40:06 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,15 @@
-import sys
-from collections import deque
-
-def main():
-    input = sys.stdin.read().split()
-    ptr = 0
-    N = int(input[ptr]); ptr +=1
-    X = int(input[ptr]); ptr +=1
-    Y = int(input[ptr]); ptr +=1
-
-    towns = []
-    for _ in range(N):
-        K = int(input[ptr]); ptr +=1
-        sweets = []
-        for __ in range(K):
-            a = int(input[ptr]); ptr +=1
-            b = int(input[ptr]); ptr +=1
-            c = int(input[ptr]); ptr +=1
-            sweets.append( (a, b, c) )
-        town
+T = int(input())
+for _ in range(T):
+    n_activities, origin = input().split()
+    n_activities = int(n_activities)
+    total = 0
+    for _ in range(n_activities):
+        activity = input().split()
+        if activity[0] == 'CONTEST_WON':
+            rank = int(activity[1])
+            bonus = max(0, 20 - rank)
+            total += 300 + bonus
+        elif activity[0] == 'TOP_CONTRIBUTOR':
+            total += 300
+        elif activity[0] == 'BUG_FOUND':
+            severity = int(activity[1])
