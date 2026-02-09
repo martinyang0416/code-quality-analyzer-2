3b3e464bfe3d1e9c86f5f2cbbb285b95a786ccec
Analyzer <analyzer@test.com>
2026-02-09 14:33:29 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,18 @@
-def smallestRangeII(A, K):
-    A.sort()
-    n = len(A)
-    ans = A[-1] - A[0]
-    for i in range(1, n):
-        left_max = A[i-1] + K
-        right_max = A[-1] - K
-        current_max = max(left_max, right_max)
-        left_min = A[0] + K
-        right_min = A[i] - K
-        current_min = min(left_min, right_min)
-        current_diff = current_max - current_min
-        if current_diff < ans:
-            ans = current_diff
-    return ans
+MOD = 10**9 + 7
+
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        current_parity = 0
+        even = 1  # Initially, prefix sum 0 is even
+        odd = 0
+        res = 0
+        for num in arr:
+            current_parity = (current_parity + num) % 2
+            if current_parity == 0:
+                res += odd
+                even += 1
+            else:
+                res += even
+                odd += 1
+            res %= MOD
+        return res % MOD
