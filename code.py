17464bc70867ddb82d2d0f5dc844fa9191670902
Analyzer <analyzer@test.com>
2026-02-09 10:46:32 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,18 @@
-import sys
-
-def to_bits(n):
-    return bin(n)[2:].zfill(2)
-
 def main():
-    lines = sys.stdin.read().splitlines()
-    N = int(lines[0])
-    result = []
-    for line in lines[1:N+1]:
-        a, b, c, d, e = map(int, line.split())
-        bits = []
-        # Convert first three numbers to 2 bits each
-        for num in [a, b, c]:
-            bits.append(to_bits(num))
-        # Convert last two numbers to 2 bits each
-        for num in [d, e]:
-            bits.append(to_bits(num))
-        combined 
+    row1 = {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'}
+    row2 = {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'}
+    row3 = {'Z', 'X', 'C', 'V', 'B', 'N', 'M'}
+
+    s = input().strip()
+
+    # Check all characters are in row1
+    if all(c in row1 for c in s):
+        print("YES")
+        return
+    # Check all characters are in row2
+    if all(c in row2 for c in s):
+        print("YES")
+        return
+    # Check all characters are in row3
+    if all(c in row3 for c in s):
+     
