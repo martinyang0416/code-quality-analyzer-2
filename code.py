3b92a3b529eef535fc0402a218e6b53bbc11fafb
Analyzer <analyzer@test.com>
2026-02-09 15:03:47 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,5 @@
-import sys
-from collections import defaultdict, deque
-
-def main():
-    input = sys.stdin.read().split()
-    idx = 0
-    N, Q = int(input[idx]), int(input[idx+1])
-    idx += 2
-
-    adj = [[] for _ in range(N+1)]
-    edges = []
-    for _ in range(N-1):
-        a = int(input[idx])
-        b = int(input[idx+1])
-        c = int(input[idx+2])
-        d = int(input[idx+3])
-        idx += 4
-        edges.append((a, b, c, d))
-        adj[a].append((b, c, d))
-        adj[b].append((a, c, d))
-
-    parent =
+# Read the number of test cases
+T = int(input())
+for _ in range(T):
+    N = int(input().split()[0])  # Read N from each test case
+    print((N // 2) + 1)
