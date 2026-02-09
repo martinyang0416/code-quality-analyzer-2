a43fce294103d521b5296cc0108c34d4f1e92fc2
Analyzer <analyzer@test.com>
2026-02-09 14:22:51 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,13 @@
-def numTrees(n):
-    dp = [0] * (n + 1)
-    dp[0] = 1
-    for i in range(1, n + 1):
-        for j in range(1, i + 1):
-            dp[i] += dp[j - 1] * dp[i - j]
-    return dp[n]
+def PredictTheWinner(nums):
+    n = len(nums)
+    dp = [[0] * n for _ in range(n)]
+    
+    for i in range(n):
+        dp[i][i] = nums[i]
+    
+    for l in range(2, n + 1):
+        for i in range(n - l + 1):
+            j = i + l - 1
+            dp[i][j] = max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1])
+    
+    return dp[0][n-1] >= 0
