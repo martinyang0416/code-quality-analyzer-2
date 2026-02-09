abd3f88e884cf700384e5c72a5864aef41016574
Analyzer <analyzer@test.com>
2026-02-09 13:45:06 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,5 @@
-from collections import defaultdict
+import math
 
-n = int(input())
-count_map = defaultdict(int)
-result = 0
-
-for _ in range(n):
-    s = input().strip()
-    reversed_s = s[::-1]
-    result += count_map[reversed_s]
-    count_map[s] += 1
-
-print(result)
+R = int(input())
+circumference = 2 * math.pi * R
+print("{0:.20f}".format(circumference))
