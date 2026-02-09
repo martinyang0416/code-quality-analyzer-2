6c97d20665290bf07eea4691a5356b6736be7c64
Analyzer <analyzer@test.com>
2026-02-09 14:19:56 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,20 @@
-def maxProfit(prices):
-    if not prices:
-        return 0
-    s0 = 0
-    s1 = -prices[0]
-    s2 = float('-inf')
-    for price in prices[1:]:
-        new_s0 = max(s0, s2)
-        new_s1 = max(s1, s0 - price)
-        new_s2 = s1 + price
-        s0, s1, s2 = new_s0, new_s1, new_s2
-    return max(s0, s2)
+def largestNumber(cost, target):
+    dp = [-float('inf')] * (target + 1)
+    dp[0] = 0
+    for i in range(1, target + 1):
+        for j in range(9):
+            c = cost[j]
+            if i >= c and dp[i - c] + 1 > dp[i]:
+                dp[i] = dp[i - c] + 1
+    if dp[target] <= 0:
+        return "0"
+    
+    res = []
+    sum_remain = target
+    digits_remain = dp[target]
+    
+    while digits_remain > 0:
+        for d in range(8, -1, -1):
+            digit = d + 1
+            c = cost[d]
+     
