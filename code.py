07c59a20189ed81271051cbd66733ded3d9fe680
Analyzer <analyzer@test.com>
2026-02-09 14:36:59 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,16 @@
-def minDays(bloomDay, m, k):
-    if m * k > len(bloomDay):
-        return -1
-    
-    left, right = min(bloomDay), max(bloomDay)
-    ans = -1
-    
-    while left <= right:
-        mid = (left + right) // 2
-        current = 0
-        bouquets = 0
-        
-        for day in bloomDay:
-            if day <= mid:
-                current += 1
-                if current == k:
-                    bouquets += 1
-                    current = 0
-            else:
-                current = 0
-        
-     
+def maxSizeSlices(slices):
+    def linear_max(arr, k):
+        m = len(arr)
+        if m < k * 2 - 1:
+            return -float('inf')
+        dp = [[-float('inf')] * (k + 1) for _ in range(m)]
+        dp[0][0] = 0
+        if k >= 1:
+            dp[0][1] = arr[0]
+        for i in range(1, m):
+            for j in range(k + 1):
+                # Not take current element
+                if dp[i - 1][j] > dp[i][j]:
+                    dp[i][j] = dp[i - 1][j]
+                # Take current element
+ 
