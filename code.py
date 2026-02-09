0121b1a645a89c8a09bb6317c03538224d3bd42d
Analyzer <analyzer@test.com>
2026-02-09 13:26:24 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,3 @@
-t = int(input())
-for _ in range(t):
-    n, m = map(int, input().split())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    b_set = set(b)
-    found = False
-    for num in a:
-        if num in b_set:
-            print("YES")
-            print(f"1 {num}")
-            found = True
-            break
-    if not found:
-        print("NO")
+a, b, c = map(int, input().split())
+t = ((a - b) * c + b - 1) // b
+print(t)
