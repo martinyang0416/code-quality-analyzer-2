3af5fea18722f7910808c5732b60db6b6157514a
Analyzer <analyzer@test.com>
2026-02-09 14:56:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,18 @@
-def canCross(stones):
-    if len(stones) < 2 or stones[0] != 0 or stones[1] != 1:
-        return False
-    stones_set = set(stones)
-    from collections import defaultdict
-    dp = defaultdict(set)
-    dp[0].add(0)
-    last_stone = stones[-1]
-    for s in stones:
-        if s not in dp:
-            continue
-        for k in dp[s]:
-            for delta in (k-1, k, k+1):
-                if delta <= 0:
-                    continue
-                next_pos = s + delta
-                if next_pos in
+from collections import deque
+from collections import defaultdict
+
+def numOfMinutes(n, headID, manager, informTime):
+    adj = defaultdict(list)
+    for i in range(n):
+        if manager[i] != -1:
+            adj[manager[i]].append(i)
+    
+    max_time = 0
+    time_informed = [0] * n
+    q = deque([headID])
+    
+    while q:
+        current = q.popleft()
+        for sub in adj.get(current, []):
+            time_informed[sub] = time_informed[current] + informTime[current]
+            if time_info
