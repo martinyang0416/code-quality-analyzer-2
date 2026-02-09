e2485e085f74d289b99d695dd78f91fda3501a09
Analyzer <analyzer@test.com>
2026-02-09 14:34:15 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,11 @@
-MOD = 10**9 + 7
-
-class Solution:
-    def numOfSubarrays(self, arr: List[int]) -> int:
-        current_parity = 0
-        even = 1  # Initially, prefix sum 0 is even
-        odd = 0
-        res = 0
-        for num in arr:
-            current_parity = (current_parity + num) % 2
-            if current_parity == 0:
-                res += odd
-                even += 1
-            else:
-                res += even
-                odd += 1
-            res %= MOD
-        return res % MOD
+def findMin(nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        elif nums[mid] < nums[right]:
+            right = mid
+        else:
+            right -= 1
+    return nums[left]
