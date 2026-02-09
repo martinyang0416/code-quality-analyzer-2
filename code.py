9dcd0f66fc6434c9e376c65b03cdae8191a46088
Analyzer <analyzer@test.com>
2026-02-09 15:13:29 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,23 @@
-import sys
+from decimal import Decimal, getcontext
 
-s = sys.stdin.read().strip()
-words = s.split()
-if not words:
-    print('')
-else:
-    min_word = min(words, key=lambda x: len(x))
-    result = [min_word]
-    for word in words:
-        result.append(word)
-        result.append(min_word)
-    print(' '.join(result))
+getcontext().prec = 20
+
+T = int(input())
+for _ in range(T):
+    m = int(input().strip())
+    percentages = input().strip().split()
+    
+    m_dec = Decimal(str(m))
+    current = m_dec
+    for p_str in percentages:
+        p = Decimal(p_str)
+        current *= (Decimal('1') + p / Decimal('100'))
+    
+    profit_percent = ((current - m_dec) / m_dec) * Decimal('100')
+    
+    if profit_percent >= 0:
+        sign = '+'
+    else:
+        sign = '-'
+    
+    ab
