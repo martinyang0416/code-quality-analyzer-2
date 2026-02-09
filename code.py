403ed1ccdd2ce0ae7323fa5f4b444777b94239a4
Analyzer <analyzer@test.com>
2026-02-09 13:38:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,16 @@
-MOD = 10**9 + 7
-MAX_N = 10**6
+n, *rest = map(int, open(0).read().split())
+p = rest[:n]
 
-# Precompute dp array
-dp = [0] * (MAX_N + 1)
-dp[0] = 1
-if MAX_N >= 1:
-    dp[1] = 1
-
-for i in range(2, MAX_N + 1):
-    dp[i] = (dp[i-1] + (i-1) * dp[i-2]) % MOD
-
-# Read input and process test cases
-import sys
-input = sys.stdin.read().split()
-T = int(input[0])
-for i in range(1, T+1):
-    N = int(input[i])
-    print(dp[N])
+count = 0
+i = 0
+while i < n:
+    if p[i] == i + 1:
+        if i + 1 < n and p[i+1] == i + 2:
+            count += 1
+            i += 2
+        else:
+            count += 1
+            i += 1
+    else:
+        i += 1
+print(count)
