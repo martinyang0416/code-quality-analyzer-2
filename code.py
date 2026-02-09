4dc35019dc4a366efb4600a47e2bd2620f527bfc
Analyzer <analyzer@test.com>
2026-02-09 10:40:32 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,19 @@
-import sys
-from collections import deque
-
 def main():
-    p = float(sys.stdin.readline())
+    import sys
     N = int(sys.stdin.readline())
-    edges = [[] for _ in range(N +1)]
-    for _ in range(N-1):
-        x,y,c = map(int, sys.stdin.readline().split())
-        edges[x].append((y, c))
-        edges[y].append((x, c))
+    temperatures = list(map(int, sys.stdin.readline().split()))
+    D = int(sys.stdin.readline())
     
-    parent = [0]*(N+1)
-    cost_to_parent = [0]*(N+1)
-    visited = [False]*(N+1)
-    q = deque([1])
-    visited[1] =True
-    parent[1]=0
-    
-    while q:
-        u = q.popleft()
-        f
+    temperatures.sort()
+    max_len = 0
+    left = 0
+    for right in range(N):
+        while temperatures[right] - temperatures[left] > D:
+            left += 1
+        current_len = right - left + 1
+        if current_len > max_len:
+            max_len = current_len
+    print(N - max_len)
+
+if __name__ == "__main__":
+    main()
