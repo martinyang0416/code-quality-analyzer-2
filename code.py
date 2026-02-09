b9db99cca060c85c099f3b511413cced4d1cb4ce
Analyzer <analyzer@test.com>
2026-02-09 14:07:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,11 @@
-import sys
-import math
-
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    t = int(data[0])
-    index = 1
-    for _ in range(t):
-        n = int(data[index])
-        m = int(data[index + 1])
-        index += 2
-        
-        a_odd = n - (n // 2)
-        a_even = n // 2
-        b_odd = m - (m // 2)
-        b_even = m // 2
-        
-        numerator = a_odd * b_even + a_even * b_odd
-        denominator = n * m
-        
-        if numerator == 0:
-            print("0/1")
-      
+n = int(input())
+total = 0
+for d in range(2, n + 1):
+    x = 2
+    while True:
+        m = d * x
+        if m > n:
+            break
+        total += 4 * x
+        x += 1
+print(total)
