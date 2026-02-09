2df35bb92ae2743c3d3313dd80bf21726e572376
Analyzer <analyzer@test.com>
2026-02-09 11:34:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,25 @@
-import sys
+import bisect
 
 def main():
-    N = int(sys.stdin.readline())
-    a = list(map(int, sys.stdin.readline().split()))
-    prefix = [0] * (N + 1)
+    import sys
+    input = sys.stdin.read().split()
+    idx = 0
+    N = int(input[idx])
+    idx += 1
+    A = list(map(int, input[idx:idx+N]))
+    idx += N
+    S = sorted(A)
+    P = [0] * (N + 1)
     for i in range(N):
-        prefix[i+1] = prefix[i] + a[i]
-
-    for i in range(1, N+1):
-        # Compute S_inc
-        s_inc = []
-        for L in range(1, i+1):
-            for R in range(i, N+1):
-                s = prefix[R] - prefix[L-1]
-                s_inc.append(s)
-        # Compute S_exc
-        s_exc = []
-        for L in range(1, N+1):
-         
+        P[i+1] = P[i] + S[i]
+    T = 0
+    for i in range(N):
+        T += S[i] * (i + 1)
+    Q = int(input[idx])
+    idx += 1
+    for _ in range(Q):
+        i = int(input[idx])
+        j = int(input[idx+1])
+        idx += 2
+        x = A[i-1]
+        pos
