45fbda9498a3d72fb2567bbbf798b2b2c1552407
Analyzer <analyzer@test.com>
2026-02-09 12:27:25 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,22 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+def giveResponse(mat,n):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if (mat[i-1][j] + mat[i+1][j] + mat[i][j-1] + mat[i][j+1])%2==1:
+                return("NO")
+    return("YES")
 
-import builtins
-import re
+mat=[]
+n = int(input())
+mat = [[0]*(n+2)]
+for i in range(n):
+    sat = [0]
+    for j in (input()):
+        if j=='o':
+            sat.append(1)
+        else:
+            sat.append(0)
+    sat.append(0)
+    mat.append(sat)
+mat.append( [0]*(n+2))
+
+print(giveResponse(mat,n))
