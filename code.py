851c4fdcccb0079e3e14538fbaaea1acb014f424
Analyzer <analyzer@test.com>
2026-02-09 10:10:35 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,16 @@
-n, *rest = map(int, open(0).read().split())
-a = rest[:n]
-
+n = int(input())
+a = list(map(int, input().split()))
 from collections import defaultdict
 
 freq = defaultdict(int)
+
 for num in a:
-    freq[num] += 1
+    msb = num.bit_length() - 1
+    freq[msb] += 1
 
-unique = list(freq.keys())
-len_unique = len(unique)
+total = 0
+for count in freq.values():
+    if count >= 2:
+        total += count * (count - 1) // 2
 
-if len_unique >= 3:
-    print(3)
-elif len_unique == 2:
-    a1, a2 = unique[0], unique[1]
-    if freq[a1] >= 2 and freq[a2] >= 2:
-        print(4)
-    else:
-        print(-1)
-else:
-    print(-1)
+print(total)
