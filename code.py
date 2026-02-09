d1915b9c7c672d7155bef25da49627866f8619ae
Analyzer <analyzer@test.com>
2026-02-09 13:32:23 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,21 @@
-n = int(input())
-a = list(map(int, input().split()))
-q = int(input())
-for _ in range(q):
-    b, e, t = map(int, input().split())
-    length = e - b
-    for k in range(length):
-        a[b + k], a[t + k] = a[t + k], a[b + k]
-print(' '.join(map(str, a)))
+import sys
+sys.setrecursionlimit(1 << 25)
+
+def main():
+    import sys
+    n, m, q = map(int, sys.stdin.readline().split())
+    total_nodes = n + m
+    parent = list(range(total_nodes + 1))  # 1-based indexing
+    rank = [1] * (total_nodes + 1)
+    
+    def find(u):
+        if parent[u] != u:
+            parent[u] = find(parent[u])
+        return parent[u]
+    
+    def union(u, v):
+        u_root = find(u)
+        v_root = find(v)
+        if u_root == v_root:
+            return
+        if rank[u_
