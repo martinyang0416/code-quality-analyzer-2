f0a4b0f74bb5148818f3f92a1bf36f0f03239c9b
Analyzer <analyzer@test.com>
2026-02-09 10:38:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,21 @@
-def can_split(p, n):
-    groups = [ [], [], [] ]
-    last = [ -1, -1, -1 ]
-    count = [0, 0, 0]
-    for idx, num in enumerate(p):
-        assigned = False
-        # Try to assign to the group with the smallest last position that is less than current index
-        # Also, don't exceed the count n for any group
-        for i in range(3):
-            if count[i] < n and last[i] < idx:
-                # Check if this group is available and can take this index
-                # We prefer the group w
+n = int(input())
+arr = list(map(int, input().split()))
+
+if n < 2:
+    print(-1)
+else:
+    max_right = arr[-1]
+    max_product = -1
+    best_a = None
+    best_b = None
+
+    for i in range(n-2, -1, -1):
+        current = arr[i]
+        if current < max_right:
+            product = current * max_right
+            if product > max_product:
+                max_product = product
+                best_a = current
+                best_b = max_right
+        # Update max_right for next iteration
+        ma
