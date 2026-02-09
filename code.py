466d61e492b422d9a8ce93a3e8de1a54efa5b3cb
Analyzer <analyzer@test.com>
2026-02-09 12:21:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,19 @@
-dumb = input()
-l = [int(o) for o in input().split()]
-x, y = [int(o) for o in input().split()]
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-def Valid(l, k, x, y):
-    s = 0
-    for i in range(k):
-        s += l[i]
-        if s >= x and s <= y and sum(l) - s >= x and sum(l) - s <= y and s != sum(l) and s != 0 and sum(l) - s != 0:
-            return True
-    return False
-
-for i in range(len(l)):
-    if Valid(l, i, x, y):
-        break
-if i+1 > 0 and i < len(l) and Valid(l, i, x, y):
-    print(i+1)
-else:
-    print(0)
+import builtins
+import re
