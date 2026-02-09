da788d2f333c03043eec32ad7f0ca3630b9bb1ee
Analyzer <analyzer@test.com>
2026-02-09 15:05:46 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,13 @@
-n = int(input())
-sticks = [tuple(map(int, input().split())) for _ in range(n)]
+import bisect
 
-prev_left = 0
-prev_right = -float('inf')
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
 
+total = 0
 for i in range(n):
-    a, h = sticks[i]
-    left_possible = False
-    if i == 0 or (a - h > sticks[i-1][0]):
-        left_possible = True
-    
-    right_possible = False
-    if i == n-1 or (a + h < sticks[i+1][0]):
-        right_possible = True
-    
-    max_prev = max(prev_left, prev_right)
-    current_left = max_prev
-    if left_possible:
-        current_left = max(current_l
+    target = a[i] + k
+    j = bisect.bisect_left(a, target, i + 1, n)
+    total += n - j
+
+print(total)
