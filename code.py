0972c091a7df3721a7a9f42cbf184b7070c47bb2
Analyzer <analyzer@test.com>
2026-02-09 13:36:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,23 @@
-n, k, M = map(int, input().split())
-t_list = list(map(int, input().split()))
-sorted_t = sorted(t_list)
-sum_t = sum(sorted_t)
-max_x = min(n, M // sum_t) if sum_t != 0 else 0
-max_points = 0
+n, K = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
 
-for x in range(max_x + 1):
-    time_used = x * sum_t
-    if time_used > M:
-        continue
-    remaining = M - time_used
-    points = x * (k + 1)
-    for tj in sorted_t:
-        possible = n - x
-        if possible <= 0:
-            continue
-        cost_per = tj
-        max_possible = possible
-        total_
+count = 0
+for i in range(n):
+    left = i + 1
+    right = n - 1
+    res = -1
+    target = a[i] + K
+    while left <= right:
+        mid = (left + right) // 2
+        if a[mid] > a[i] and a[mid] <= target:
+            res = mid
+            right = mid - 1
+        elif a[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    if res != -1:
+        count += 1
+
+print(n - count)
