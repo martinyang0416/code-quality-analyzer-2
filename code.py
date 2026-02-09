c67199c4189fe524721bb7725d4941ccd0410a4b
Analyzer <analyzer@test.com>
2026-02-09 11:29:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,23 @@
 from collections import deque
 
 class Edge:
+    __slots__ = ('to', 'rev', 'capacity')
     def __init__(self, to, rev, capacity):
         self.to = to
         self.rev = rev
         self.capacity = capacity
 
-class Dinic:
-    def __init__(self, n):
-        self.size = n
-        self.graph = [[] for _ in range(n)]
-        
-    def add_edge(self, fr, to, capacity):
-        forward = Edge(to, len(self.graph[to]), capacity)
-        backward = Edge(fr, len(self.graph[fr]), 0)
-        self.graph[fr].append(forward)
-        self.graph[to].append(
+def max_flow(adj, s, t):
+    N = len(adj)
+    level = [0] * N
+    ptr = [0] * N
+
+    def bfs():
+        q = deque()
+        level[:] = [-1] * N
+        level[s] = 0
+        q.append(s)
+        while q:
+            v = q.popleft()
+            for edge in adj[v]:
+                if edge.capacity
