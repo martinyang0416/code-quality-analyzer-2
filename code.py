c7899092aeb9f6aac102e14166e1ff5a9cf9eddf
Analyzer <analyzer@test.com>
2026-02-09 13:48:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,17 @@
-n, L = map(int, input().split())
-intervals = [tuple(map(int, input().split())) for _ in range(n)]
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    idx = 0
+    T = int(input[idx])
+    idx += 1
+    for _ in range(T):
+        N, M = int(input[idx]), int(input[idx+1])
+        idx +=2
+        A = list(map(int, input[idx:idx+M]))
+        idx += M
+        count_ones = A.count(1)
+        required = M - 1 - count_ones
+        print(max(required, 0))
 
-# Compute x
-sorted_intervals = sorted(intervals, key=lambda x: (x[0], -x[1]))
-current_end = 0
-count = 0
-i = 0
-possible = True
-
-while current_end < L:
-    max_r = current_end
-    while i < len(sorted_intervals) and sorted_intervals[i][0] <= current_end:
-        if sorted_intervals[i][1] > max_r:
-            max_r = sorted_intervals[i][1]
-        i += 1
-    if max_r == current_end:
-        possible =
+if __name__ == "__main__":
+    main()
