f8f459a107f9fd40ca987d3caec6999bfb9224b8
Analyzer <analyzer@test.com>
2026-02-09 13:25:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,15 @@
-import sys
-
-n = int(sys.stdin.readline())
-dp = [-float('inf')] * 10
-dp[0] = 0
-
-for _ in range(n):
-    k = int(sys.stdin.readline())
-    cards = []
-    for __ in range(k):
-        c, d = map(int, sys.stdin.readline().split())
-        cards.append((c, d))
-    
-    c1, c2, c3 = [], [], []
-    for c, d in cards:
-        if c == 1:
-            c1.append(d)
-        elif c == 2:
-            c2.append(d)
-        elif c == 3:
-            c3.append(d)
-    c1.sort(reverse=True)
-    c2.sort(reverse=True)
-  
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    b_set = set(b)
+    found = False
+    for num in a:
+        if num in b_set:
+            print("YES")
+            print(f"1 {num}")
+            found = True
+            break
+    if not found:
+        print("NO")
