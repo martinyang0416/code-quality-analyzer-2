5bc359805a681f586bfa76ee3c42cc5a17e6c403
Analyzer <analyzer@test.com>
2026-02-09 14:07:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,24 @@
-n = int(input())
-total = 0
-for d in range(2, n + 1):
-    x = 2
-    while True:
-        m = d * x
-        if m > n:
-            break
-        total += 4 * x
-        x += 1
-print(total)
+n, m, k = map(int, input().split())
+p = list(map(int, input().split()))
+
+shift = 0
+operations = 0
+i = 0
+
+while i < m:
+    current_p = p[i] - shift
+    current_page = (current_p - 1) // k
+    upper_bound = (current_page + 1) * k + shift
+    
+    low = i
+    high = m - 1
+    best = i - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if p[mid] <= upper_bound:
+            best = mid
+            low = mid + 1
+        else:
+            high = mid - 1
+    j = best + 1
+    operations +=
