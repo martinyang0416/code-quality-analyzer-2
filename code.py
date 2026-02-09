0fec3b606817607f6d96703c5d9c750be09c8a25
Analyzer <analyzer@test.com>
2026-02-09 10:31:46 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,22 @@
-def main():
+def minimal_max():
     import sys
     input = sys.stdin.read().split()
-    idx = 0
-    N = int(input[idx])
-    idx += 1
-    K = int(input[idx])
-    idx += 1
-    D = int(input[idx])
-    idx += 1
-
-    times = []
-    for _ in range(N):
-        time_str = input[idx]
-        idx += 1
-        hh, mm, ss = map(int, time_str.split(':'))
-        t = hh * 3600 + mm * 60 + ss
-        times.append(t)
-
-    events = []
-    for t in times:
-        start = t
-        end = t + D
-        events.append((start, 1))
-      
+    
+    n = int(input[0])
+    sequence = list(map(int, input[1:n+1]))
+    
+    # Group into runs
+    runs = []
+    if not sequence:
+        print(0)
+        return
+    current_x = sequence[0]
+    current_m = 1
+    for num in sequence[1:]:
+        if num == current_x:
+            current_m += 1
+        else:
+            runs.append((current_x, current_m))
+            current_x = num
+            current_m = 1
+    runs.append((
