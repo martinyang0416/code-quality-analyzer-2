1ddbfe5091f43fc2bde2a51fe7b2b645933f2290
Analyzer <analyzer@test.com>
2026-02-09 14:06:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,24 @@
 import sys
-from collections import deque
-
-MOD = 10**9 + 7
+import math
 
 def main():
-    input = sys.stdin.read().split()
-    ptr = 0
-    N = int(input[ptr])
-    ptr += 1
-    words = []
-    for _ in range(N):
-        words.append(input[ptr])
-        ptr += 1
-    t = input[ptr]
-    ptr += 1
-
-    # Build the trie
-    nodes = [{'children': {}, 'failure': -1, 'output': [], 'combined_output': []}]
-
-    for word in words:
-        current = 0
-        for c in word:
-            if c not in nodes[current]['children']:
+    input = sys.stdin.read
+    data = input().split()
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        n = int(data[index])
+        m = int(data[index + 1])
+        index += 2
+        
+        a_odd = n - (n // 2)
+        a_even = n // 2
+        b_odd = m - (m // 2)
+        b_even = m // 2
+        
+        numerator = a_odd * b_even + a_even * b_odd
+        denominator = n * m
+        
+        if numerator == 0:
+            print("0/1")
+      
