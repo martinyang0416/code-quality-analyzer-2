c954e402172811eff874fbbb91f5a50ef620ae76
Analyzer <analyzer@test.com>
2026-02-09 10:12:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,21 @@
-MOD = 998244353
+MOD = 10**9 + 7
 
 n = int(input())
-# The array is read but not used since the sum is n*(n+1)/2 regardless of the fixed values
-input().split()
+c = list(map(int, input().split()))
+d = list(map(int, input().split()))
+q = int(input())
+y_list = list(map(int, input().split()))
 
-n_mod = n % MOD
-sum_n = (n_mod * (n + 1)) % MOD
-inv_two = pow(2, MOD-2, MOD)
-result = (sum_n * inv_two) % MOD
+max_a1 = c[0] if n > 0 else 0
+counts = [0] * (max_a1 + 1)
 
-print(result)
+def compute_count(a1):
+    if a1 < 0 or a1 > c[0]:
+        return 0
+    current = [0] * (c[0] + 1)
+    current[a1] = 1
+    for i in range(n-1):
+        next_dp = [0] * (c[i+1] + 1)
+        d_val = d[i]
+        for a_prev in range(len(current)):
+            if current[a_prev] 
