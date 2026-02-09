022c986299dcd78e2569d7163d24ef6ed63b67ec
Analyzer <analyzer@test.com>
2026-02-09 11:26:51 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,16 @@
 def putaway(A, B, T, X, Y, W, S):
-    max_X = -float('inf')
-    if A > 0:
-        max_X = max(X)
-    max_Y = -float('inf')
-    if B > 0:
-        max_Y = max(Y)
-
-    W_only = 0
-    S_only = 0
-    Both = 0
-
+    # Check if each toy can be handled by at least one robot
     for i in range(T):
-        w = W[i]
-        s = S[i]
-        weak_ok = (w < max_X)
-        small_ok = (s < max_Y)
-        if weak_ok and small_ok:
-            Both += 1
-        elif weak_ok:
-            W_only += 1
-        elif small_ok:
-            S_only += 1
-        else:
-            ret
+        possible = False
+        # Check weak robots
+        if A > 0:
+            for x in X:
+                if W[i] < x:
+                    possible = True
+                    break
+        # Check small robots if not possible yet
+        if not possible and B > 0:
+            for y in Y:
+                if S[i] < y:
+                    possible = True
+                    bre
