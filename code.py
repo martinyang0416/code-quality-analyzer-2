b8fe54853a2e9e26cb624ce978504dbf5f560e6a
Analyzer <analyzer@test.com>
2026-02-09 13:24:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,4 @@
-n = int(input())
-A = list(map(int, input().split()))
-swap_count = 0
-
-for i in range(n):
-    mini = i
-    for j in range(i, n):
-        if A[j] < A[mini]:
-            mini = j
-    if i != mini:
-        A[i], A[mini] = A[mini], A[i]
-        swap_count += 1
-
-print(' '.join(map(str, A)))
-print(swap_count)
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    print((N // 2) + 1)
