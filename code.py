c6ff2d49de796389413878ebf9ca35031db3ee8e
Analyzer <analyzer@test.com>
2026-02-09 13:58:01 -0800

    Update

--- code.py
+++ code.py
@@ -1,30 +1,10 @@
-MOD = 10**9 + 7
-
-S = input().strip()
-T = input().strip()
-n = len(S)
-
-current_lt = 0
-current_lt_sum = 0
-current_eq = 1
-current_eq_sum = 0
-
-for i in range(n):
-    s_digit = int(S[i])
-    t_char = T[i]
-    
-    if t_char == '?':
-        digits = list(range(10))
-    else:
-        digits = [int(t_char)]
-    
-    new_lt = 0
-    new_lt_sum = 0
-    new_eq = 0
-    new_eq_sum = 0
-    
-    # Process transitions from eq state
-    if current_eq > 0:
-        for d in digits:
-            if d < s_digit:
-      
+T = int(input())
+for _ in range(T):
+    N, K = map(int, input().split())
+    forgotten_words = input().split()
+    modern_set = set()
+    for _ in range(K):
+        parts = input().split()
+        modern_set.update(parts[1:])
+    result = ['YES' if word in modern_set else 'NO' for word in forgotten_words]
+    print(' '.join(result))
