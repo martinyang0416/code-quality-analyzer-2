3d043def7b83e9f21f1cc7e98d9d9ae007ee2e3a
Analyzer <analyzer@test.com>
2026-02-09 10:24:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,22 @@
-n = int(input())
-nums = list(map(int, input().split()))
-evens = []
-odds = []
-for num in nums:
-    if num % 2 == 0:
-        evens.append(num)
+n, m = map(int, input().split())
+c = list(map(int, input().split()))
+
+cnt = [0] * m
+for num in c:
+    rem = num % m
+    cnt[rem] += 1
+
+total_pairs = 0
+
+# Handle remainder 0
+total_pairs += cnt[0] // 2
+
+# Iterate through possible remainders
+for r in range(1, (m // 2) + 1):
+    if r != m - r:
+        total_pairs += min(cnt[r], cnt[m - r])
     else:
-        odds.append(num)
-result = evens + odds
-print(' '.join(map(str, result)))
+        # Handle the case where r is exactly half of m (even m)
+        total_pairs += (cnt[r] // 2)
+
+print(total_pairs * 2)
