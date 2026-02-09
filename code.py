6bfdf456c657a34a74e3c7297844662a65b6fff5
Analyzer <analyzer@test.com>
2026-02-09 11:22:43 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,15 @@
-import sys
+a_part = "What are you doing while sending "  # Length 33
+b_part = " Are you busy? Will you send "      # Length 29
+f0 = "What are you doing at the end of the world? Are you busy? Will you save us?"  # Length 75
 
-for _ in sys.stdin:
-    print("NO")
-    sys.stdout.flush()
+def solve():
+    import sys
+    q = int(sys.stdin.readline())
+    for _ in range(q):
+        n, k = map(int, sys.stdin.readline().split())
+        current_level = n
+        current_k = k
+        while True:
+            if current_level == 0:
+                if current_k > 75:
+          
