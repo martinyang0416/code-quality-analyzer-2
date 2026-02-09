8fad32ba08fa0e5204343824f2f97960461c198b
Analyzer <analyzer@test.com>
2026-02-09 14:28:13 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,17 @@
-def min_good(fronts, backs):
-    candidates = sorted(set(fronts + backs))
-    for x in candidates:
-        conflict = False
-        for f, b in zip(fronts, backs):
-            if f == x and b == x:
-                conflict = True
-                break
-        if conflict:
-            continue
-        for f, b in zip(fronts, backs):
-            if (b == x and f != x) or (f == x and b != x):
-                return x
-    return 0
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        if rows == 0:
+            return 0
+        cols = len(grid[0])
+        if cols < 3 or rows < 3:
+            return 0
+        count = 0
+        for i in range(rows - 2):
+            for j in range(cols - 2):
+                sub = [
+                    grid[i][j:j+3],
+                    grid[i+1][j:j+3],
+                    grid[i+2][j:j+3]
+                ]
+                if sub[1
