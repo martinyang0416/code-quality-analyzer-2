6e948d4bcfafdb9bc50808577e086ace5bf9ea96
Analyzer <analyzer@test.com>
2026-02-09 13:26:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,3 +1,21 @@
-a, b, c = map(int, input().split())
-t = ((a - b) * c + b - 1) // b
-print(t)
+n, x = map(int, input().split())
+a = list(map(int, input().split()))
+
+roots = {}
+depths = {}
+
+for node in range(1, n+1):
+    if node in roots:
+        continue
+    path = []
+    current = node
+    while True:
+        if current in roots:
+            root = roots[current]
+            depth_current = depths[current]
+            for nd in reversed(path):
+                roots[nd] = root
+                depth_current += 1
+                depths[nd] = depth_current
+            break
+        predecess
