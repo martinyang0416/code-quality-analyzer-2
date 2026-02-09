d6bf33a5f12808c97eadbd0aab4257002e97a76e
Analyzer <analyzer@test.com>
2026-02-09 10:05:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,16 @@
-n = int(input())
-t = input().strip()
+n, m = map(int, input().split())
+degrees = [0] * (n + 1)
+for _ in range(m):
+    u, v = map(int, input().split())
+    degrees[u] += 1
+    degrees[v] += 1
 
-s = []
-index = 0
-group_size = 1
+min_modifications = float('inf')
 
-while index < len(t):
-    # Extract the group of current group_size
-    group = t[index:index + group_size]
-    # Append the first character of the group to s
-    s.append(group[0])
-    # Move to the next group
-    index += group_size
-    group_size += 1
+for h in range(1, n + 1):
+    c = degrees[h]
+    mod = (n - 1) + m - 2 * c
+    if mod < min_modifications:
+        min_modifications = mod
 
-print(''.join(s))
+print(min_modifications)
