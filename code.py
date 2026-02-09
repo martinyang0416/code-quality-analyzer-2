d1de2d19dfb8db9876ddb3384a97f222c629f254
Analyzer <analyzer@test.com>
2026-02-09 10:42:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,23 @@
-m = int(input())
-if m > 0:
-    for i in range(m):
-        row = []
-        for j in range(m):
-            d1 = i
-            d2 = (m - 1) - i
-            d3 = j
-            d4 = (m - 1) - j
-            min_d = min(d1, d2, d3, d4)
-            row.append(str(min_d))
-        print(' '.join(row))
+from heapq import heappush, heappop
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    idx = 0
+    n = int(data[idx])
+    idx += 1
+    m = int(data[idx])
+    idx += 1
+    q = int(data[idx])
+    idx += 1
+
+    types = list(map(int, data[idx:idx + n]))
+    idx += n
+
+    unique_types = list(sorted(set(types)))
+    type_to_idx = {t: i for i, t in enumerate(unique_types)}
+    T = len(unique_types)
+
+    adj = [[] for _ in range(n + 1)]  # 1-based
+    for _ in range(m)
