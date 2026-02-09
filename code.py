f84c525abf4163adf81083b709fefec217ee9a9f
Analyzer <analyzer@test.com>
2026-02-09 11:28:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,18 @@
-import sys
 from collections import deque
+
+class Edge:
+    def __init__(self, to, rev, capacity):
+        self.to = to
+        self.rev = rev
+        self.capacity = capacity
 
 class Dinic:
     def __init__(self, n):
         self.size = n
         self.graph = [[] for _ in range(n)]
         
-    def add_edge(self, fr, to, cap):
-        forward = [to, len(self.graph[to]), cap]
-        backward = [fr, len(self.graph[fr]), 0]
+    def add_edge(self, fr, to, capacity):
+        forward = Edge(to, len(self.graph[to]), capacity)
+        backward = Edge(fr, len(self.graph[fr]), 0)
         self.graph[fr].append(forward)
-        self.graph[to].append(backward)
-        
-    def bfs_level(self, s, t, level):
-        q = deque()
-        level[:] = [-1] * self.size
-        level[s] = 0
-      
+        self.graph[to].append(
