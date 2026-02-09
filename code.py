daee56bbb91e749899c2e5f923e679d5eaa5556d
Analyzer <analyzer@test.com>
2026-02-09 12:58:40 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,22 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+def solve():
+    n=int(input())
+    s=sorted(input(), key=(lambda x: ord(x)))
+    ind=[[s[0],1]]
+    if len(s) % n!=0:
+        print(-1)
+        return
+    for i in range(1,len(s)):
+        if s[i]!=ind[len(ind)-1][0]:
+            ind.append([s[i],1])
+        else: ind[len(ind)-1][1]+=1
+    ans=[]
+    for ii in ind:
+        if ii[1] % n!=0:
+            print(-1)
+            return
+        ans.extend([ii[0] for i in range(ii[1]//n)])
+    print("".join(ans * n))
+solve()
 
-import builtins
-import re
+
+          
