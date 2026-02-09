666d211c7b5053fefb1f4cc04731159229623f40
Analyzer <analyzer@test.com>
2026-02-09 10:24:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,11 @@
-import sys
-
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    idx = 0
-    t = int(data[idx])
-    idx += 1
-    for _ in range(t):
-        n = int(data[idx])
-        idx += 1
-        a = list(map(int, data[idx:idx+n]))
-        idx += n
-        c = list(map(int, data[idx:idx+n]))
-        idx += n
-        c_sorted = sorted(c)
-        d = [a[i] - c_sorted[i] for i in range(n)]
-        print(' '.join(map(str, d)))
-        print(' '.join(map(str, d)))
-
-if __name__ == '__main__':
-   
+n = int(input())
+nums = list(map(int, input().split()))
+evens = []
+odds = []
+for num in nums:
+    if num % 2 == 0:
+        evens.append(num)
+    else:
+        odds.append(num)
+result = evens + odds
+print(' '.join(map(str, result)))
