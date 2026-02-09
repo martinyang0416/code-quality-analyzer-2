14f5dd36b924450c92888a636530af45236a93a8
Analyzer <analyzer@test.com>
2026-02-09 14:15:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,18 @@
-def num_rescue_boats(people, limit):
-    people.sort()
-    left = 0
-    right = len(people) - 1
-    boats = 0
-    while left <= right:
-        if people[left] + people[right] <= limit:
-            left += 1
-        right -= 1
-        boats += 1
-    return boats
+import heapq
+
+def maxPerformance(n, speed, efficiency, k):
+    engineers = sorted(zip(efficiency, speed), key=lambda x: -x[0])
+    sum_speed = 0
+    heap = []
+    max_perf = 0
+    MOD = 10**9 + 7
+    
+    for eff, spd in engineers:
+        current = spd + sum_speed
+        current_perf = current * eff
+        if current_perf > max_perf:
+            max_perf = current_perf
+        heapq.heappush(heap, spd)
+        sum_speed += spd
+        if len(heap) > k - 1:
+            removed = heapq.heappop(
