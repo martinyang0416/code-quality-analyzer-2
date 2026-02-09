5087452e6e9d6af7a820df5c347d8f1028ec4270
Analyzer <analyzer@test.com>
2026-02-09 15:03:02 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,22 @@
-def find_single(nums):
-    low = 0
-    high = len(nums) - 1
-    while low < high:
-        mid = (low + high) // 2
-        if mid % 2 == 0:
-            if nums[mid] == nums[mid + 1]:
-                low = mid + 2
-            else:
-                high = mid
-        else:
-            if nums[mid] == nums[mid - 1]:
-                low = mid + 1
-            else:
-                high = mid
-    return nums[low]
+import sys
+from collections import defaultdict, deque
+
+def main():
+    input = sys.stdin.read().split()
+    idx = 0
+    N, Q = int(input[idx]), int(input[idx+1])
+    idx += 2
+
+    adj = [[] for _ in range(N+1)]
+    edges = []
+    for _ in range(N-1):
+        a = int(input[idx])
+        b = int(input[idx+1])
+        c = int(input[idx+2])
+        d = int(input[idx+3])
+        idx += 4
+        edges.append((a, b, c, d))
+        adj[a].append((b, c, d))
+        adj[b].append((a, c, d))
+
+    parent =
