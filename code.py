ff585c9d7c9ef3c0ad376766d995c830c7ae7393
Analyzer <analyzer@test.com>
2026-02-09 13:46:19 -0800

    Update

--- code.py
+++ code.py
@@ -1,8 +1,21 @@
-# Read the input values
-A, B, C = map(int, input().split())
+groups = [
+    ['a', 'b', 'c', 'd', 'e'],
+    ['f', 'g', 'h', 'i', 'j'],
+    ['k', 'l', 'm', 'n', 'o'],
+    ['p', 'q', 'r', 's', 't'],
+    ['u', 'v', 'w', 'x', 'y'],
+    ['z', '.', '?', '!', ' ']
+]
 
-# Check if the sum of A and B is at least C
-if A + B >= C:
-    print("Yes")
-else:
-    print("No")
+def convert(s):
+    if len(s) % 2 != 0:
+        return "NA"
+    res = []
+    for i in range(0, len(s), 2):
+        pair = s[i:i+2]
+        if len(pair) != 2:
+            return "NA"
+        a, b = pair[0], pair[1]
+        if a not in '123456' or b not in '12345':
+            return "NA"
+        group
