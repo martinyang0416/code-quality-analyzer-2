5a684b244f2430400526034940eca25913548014
Analyzer <analyzer@test.com>
2026-02-09 14:14:01 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,8 @@
-def maxScoreSightseeingPair(A):
-    max_so_far = A[0] + 0  # A[i] + i for i=0
-    result = 0
-    for j in range(1, len(A)):
-        current_score = max_so_far + (A[j] - j)
-        if current_score > result:
-            result = current_score
-        # Update max_so_far for next iterations
-        current_i_value = A[j] + j
-        if current_i_value > max_so_far:
-            max_so_far = current_i_value
-    return result
+def maxIncreaseKeepingSkyline(grid):
+    row_max = [max(row) for row in grid]
+    col_max = [max(col) for col in zip(*grid)]
+    total = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            total += min(row_max[i], col_max[j]) - grid[i][j]
+    return total
