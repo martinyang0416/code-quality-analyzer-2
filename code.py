2ee9f39c3a4885ebe8ef80490b9cec268c6f21ea
Analyzer <analyzer@test.com>
2026-02-09 12:24:59 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,19 @@
-from math import ceil
-def gcdEx(a,b,x,y):
-    if not a:
-        return 0,1,b
-    x1,y1,g=gcdEx(b%a,a,0,0)
-    x=y1-(b//a)*x1
-    y=x1
-    return x,y,g
-a,b,c=map(int, input().split())
-x,y,g=gcdEx(a,b,0,0)
-if c%g:
-    print("No")
-else:    
-    x,y=x*c//g,y*c//g
-    k1=ceil(-x*g/b)
-    k2=(y*g)//a
-    c=abs(k2-k1+1)
-    print("Yes" if c>0 else "No")
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
+
+import builtins
+import re
