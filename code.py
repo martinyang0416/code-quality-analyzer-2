50ab0474dcc91365f99220cb39a7c2aa58b38c25
Analyzer <analyzer@test.com>
2026-02-09 14:24:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,17 @@
-def minNumberOfFrogs(croakOfFrogs):
-    counts = {'c': 0, 'r': 0, 'o': 0, 'a': 0}
-    max_frogs = 0
-    for char in croakOfFrogs:
-        if char == 'c':
-            counts['c'] += 1
-        elif char == 'r':
-            if counts['c'] == 0:
-                return -1
-            counts['c'] -= 1
-            counts['r'] += 1
-        elif char == 'o':
-            if counts['r'] == 0:
-                return -1
-            counts['r'] -= 1
-            counts['o'] += 1
-        elif char == 'a':
-     
+import bisect
+
+def lenLongestFibSubseq(A):
+    n = len(A)
+    max_len = 0
+    dp = [[2] * n for _ in range(n)]
+    index = {x: i for i, x in enumerate(A)}
+    
+    for k in range(n):
+        for j in range(k):
+            target = A[k] - A[j]
+            if target in index and index[target] < j:
+                i = index[target]
+                dp[j][k] = dp[i][j] + 1
+                if dp[j][k] > max_len:
+                    max_len = dp[j][k]
+    return max_len if max_len >= 3 else 0
