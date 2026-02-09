07f7a83178f83cb3cc5579adc21de11daef37e9f
Analyzer <analyzer@test.com>
2026-02-09 10:32:13 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,18 @@
-def minimal_max():
-    import sys
-    input = sys.stdin.read().split()
-    
-    n = int(input[0])
-    sequence = list(map(int, input[1:n+1]))
-    
-    # Group into runs
-    runs = []
-    if not sequence:
-        print(0)
-        return
-    current_x = sequence[0]
-    current_m = 1
-    for num in sequence[1:]:
-        if num == current_x:
-            current_m += 1
-        else:
-            runs.append((current_x, current_m))
-            current_x = num
-            current_m = 1
-    runs.append((
+n, d = map(int, input().split())
+values = list(map(int, input().split()))
+times = list(map(int, input().split()))
+tasks = list(zip(values, times))
+
+def calculate_score(sorted_tasks, d):
+    total = 0
+    current_time = 0
+    for v, t in sorted_tasks:
+        finish_time = current_time + t
+        total += v - d * finish_time
+        current_time = finish_time
+    return total
+
+alice_sorted = sorted(tasks, key=lambda x: x[0])
+bob_sorted = sorted(tasks, key=lambda x: -x[0])
+
+alice_score = calculat
