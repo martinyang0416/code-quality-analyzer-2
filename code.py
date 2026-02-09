4bcf260fd19ec951311aa205e0a0544c09f1f60e
Analyzer <analyzer@test.com>
2026-02-09 11:16:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 a1, a2, a3 = map(int, input().split())
+target = a3 + 1
 terms = [a1, a2]
-for _ in range(a3 - 1):
+for i in range(2, target):
     next_term = terms[-1] + terms[-2]
     terms.append(next_term)
 print(terms[-1])
