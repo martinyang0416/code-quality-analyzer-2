1cb5416d6f67d2240ecb4bf7706c24fcadc023e4
Analyzer <analyzer@test.com>
2026-02-09 13:54:25 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,12 @@
-s, v1, v2, t1, t2 = map(int, input().split())
-time1 = 2 * t1 + s * v1
-time2 = 2 * t2 + s * v2
-if time1 < time2:
-    print("First")
-elif time2 < time1:
-    print("Second")
-else:
-    print("Friendship")
+n = int(input())
+root = {}
+count = 0
+for _ in range(n):
+    word = input().strip()
+    current = root
+    for c in word:
+        if c not in current:
+            current[c] = {}
+            count += 1
+        current = current[c]
+print(count)
