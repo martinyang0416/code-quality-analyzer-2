347d1c36b8f357e53048dd782427ad29063d3a3a
Analyzer <analyzer@test.com>
2026-02-09 10:35:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,25 @@
+import sys
 import bisect
 
-def main():
-    import sys
-    m, V = map(int, sys.stdin.readline().split())
-    E = list(map(int, sys.stdin.readline().split()))
-    max_efficiency = -1.0
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
     
-    for p in range(m - 2):
-        target = E[p] + V
-        r = bisect.bisect_right(E, target) - 1
-        
-        if r >= p + 2:
-            numerator = E[r] - E[p + 1]
-            denominator = E[r] - E[p]
-            
-            if denominator == 0:
-                continue
-            
-            efficiency = numer
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        fx = self.find(x)
+        fy = self.find(y)
+        if fx != fy:
+            self.parent[fy] = fx
+
+def main():
+    input = sys.stdin.read
+    data = input().split()
+    idx = 0
+    t = int(data[idx])
+    idx += 1
+ 
