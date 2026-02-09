031cd535287fcb9564e1a7766aa3135a7d927abd
Analyzer <analyzer@test.com>
2026-02-09 14:45:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,16 @@
-MOD = 10**9 + 7
-
-def maxSum(nums1, nums2):
-    # Compute prefix sums for both arrays
-    prefix1 = [0]
-    for num in nums1:
-        prefix1.append(prefix1[-1] + num)
-    
-    prefix2 = [0]
-    for num in nums2:
-        prefix2.append(prefix2[-1] + num)
-    
-    # Find common elements along with their indices in both arrays
-    commons = []
-    i = j = 0
-    n1, n2 = len(nums1), len(nums2)
-    while i < n1 and j < n2:
-        a, b = nums1[i], nums2[j]
-        if a == b:
-            commons.appen
+class Solution:
+    def sumSubseqWidths(self, A: List[int]) -> int:
+        MOD = 10**9 + 7
+        A.sort()
+        n = len(A)
+        pow2 = [1] * n
+        for i in range(1, n):
+            pow2[i] = (pow2[i-1] * 2) % MOD
+        
+        sum_max = 0
+        sum_min = 0
+        for i in range(n):
+            sum_max = (sum_max + A[i] * pow2[i]) % MOD
+            sum_min = (sum_min + A[i] * pow2[n - 1 - i]) % MOD
+        
+        return (sum_max - sum_min) % MOD
