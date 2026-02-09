1a80bb18ca2df71af9eae92f318f0b3d6b6ac006
Analyzer <analyzer@test.com>
2026-02-09 14:43:06 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,8 @@
-from typing import List
-
-class Solution:
-    def minFallingPathSum(self, A: List[List[int]]) -> int:
-        n = len(A)
-        if n == 0:
-            return 0
-        prev_row = A[0].copy()
-        for i in range(1, n):
-            current_row = []
-            for j in range(n):
-                possible = []
-                if j > 0:
-                    possible.append(prev_row[j-1])
-                possible.append(prev_row[j])
-                if j < n - 1:
-                    possible.append(p
+def canMeasureWater(x, y, z):
+    if z == 0:
+        return True
+    if x + y < z:
+        return False
+    from math import gcd
+    g = gcd(x, y)
+    return z % g == 0
