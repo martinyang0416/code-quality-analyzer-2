adf95ea48b9164c463124ded045e25ab208c6a7a
Analyzer <analyzer@test.com>
2026-02-09 14:22:09 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,7 @@
-def maxSubarraySumCircular(A):
-    # Compute maximum subarray sum using Kadane's algorithm
-    max_current = max_so_far = A[0]
-    for num in A[1:]:
-        max_current = max(num, max_current + num)
-        max_so_far = max(max_so_far, max_current)
-    
-    # If all elements are negative, return the maximum element (handled by max_so_far)
-    if max_so_far < 0:
-        return max_so_far
-    
-    # Compute minimum subarray sum using Kadane's algorithm for min
-    min_current = min_so_far = A[0]
- 
+def numTrees(n):
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            dp[i] += dp[j - 1] * dp[i - j]
+    return dp[n]
