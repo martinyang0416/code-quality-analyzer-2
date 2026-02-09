73ad5786c1fdf3a4d162967bc652a4bc381941da
Analyzer <analyzer@test.com>
2026-02-09 13:55:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,11 @@
-n = int(input())
-root = {}
-count = 0
-for _ in range(n):
-    word = input().strip()
-    current = root
-    for c in word:
-        if c not in current:
-            current[c] = {}
-            count += 1
-        current = current[c]
-print(count)
+t = int(input())
+for _ in range(t):
+    n, c, m = map(int, input().split())
+    initial = n // c
+    total = initial
+    wrappers = initial
+    while wrappers >= m:
+        exchange = wrappers // m
+        total += exchange
+        wrappers = wrappers % m + exchange
+    print(total)
