81441f7ccbfedcdd6a6ac33eca476c24d7040d57
Analyzer <analyzer@test.com>
2026-02-09 10:19:30 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,20 @@
-import math
-
-def max_sum_after_flips(t, test_cases):
+def find_dominant_tree(t, test_cases):
     results = []
     for case in test_cases:
-        n, m, a, B = case
-        sum_initial = sum(a)
-        sum_abs = sum(abs(num) for num in a)
-        B = set(B)
-        
-        if 1 in B:
-            results.append(sum_abs)
+        n, h = case
+        if n == 1:
+            results.append(1)
             continue
         
-        has_odd = any(x % 2 != 0 for x in B)
-        if has_odd:
-            results.append(sum_abs)
-            continue
+        left = [0] * n
+        for i in range(1, n):
+            if h[i-1] < h[i]:
+                left[i] = left[i-1] + 1
+            else:
+                left[i] = 0
         
-        negatives = [num for num in a if num < 0]
-        
+        right = [0] * n
+        for i in range(n-2, -1, -1):
+            if h[i] < h[i+1]:
+                right[i] = right[i+1] + 1
+      
