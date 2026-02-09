17bcfbdd52ef4bd1125772e3010551adc6e41101
Analyzer <analyzer@test.com>
2026-02-09 13:02:33 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,5 @@
-import math
-
 n = int(input())
-for _ in range(n):
-    a_str, b_str = input().split()
-    a = int(a_str)
-    if a == 0:
-        if all(c == '0' for c in b_str):
-            print(0)
-        else:
-            print(int(b_str))
-    else:
-        mod = 0
-        for c in b_str:
-            mod = (mod * 10 + int(c)) % a
-        print(math.gcd(a, mod))
+s = input().strip()
+count_8 = s.count('8')
+max_phones = n // 11
+print(min(count_8, max_phones))
