4fd5af231d8da699306fac09c4dc406e58d9d91a
Analyzer <analyzer@test.com>
2026-02-09 11:56:48 -0800

    Update

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ import sys
 from collections import deque
 
 def rotate_right(s, N):
-    return ((s >> 1) | ((s & 1) << (N-1))) & ((1 << N) - 1)
+    return ( (s >> 1) | ( (s & 1) << (N-1) ) )
 
 def main():
     input = sys.stdin.read().split()
@@ -11,14 +11,16 @@ def main():
     idx += 1
     N = int(input[idx])
     idx += 1
-
-    for _ in range(T):
-        lights_str = input[idx]
-        switches_str = input[idx+1]
-        idx +=2
-
-        initial_lights = int(lights_str, 2)
-        initial_switches = int(switches_str, 2)
-        target = initial_lights
-
     
+    for _ in range(T):
+        L_str = input[idx]
+        S_str = input[idx+1]
+        idx +=2
+        
+        L = int(L_str, 2)
+        S = int(S_str, 2)
+        target = L
+        
+        visited = {}
+        q = deque()
+        initial = (S,
