5cd8e4a87ad4ab37a1fe91dbea0be09f722b2541
Analyzer <analyzer@test.com>
2026-02-09 10:21:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,22 @@
-L, m = map(int, input().split())
-# Discard the list of checkpoints as they are not needed
-input()
-s, t = map(int, input().split())
-distance = abs(t - s)
-min_distance = min(distance, L - distance)
-print(min_distance)
+import sys
+from sys import stdin
+
+def main():
+    sys.setrecursionlimit(1 << 25)
+    n = int(stdin.readline())
+    adj = [[] for _ in range(n + 1)]
+    for _ in range(n - 1):
+        u, v = map(int, stdin.readline().split())
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    parent = [0] * (n + 1)
+    parent[1] = -1  # Root has no parent
+    size = [1] * (n + 1)
+    T = 0
+    stack = [(1, False)]
+    
+    while stack:
+        node, visited = stack.pop()
+        if not visited:
+          
