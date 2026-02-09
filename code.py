c7903b17aa9639acdee2cdbcaa3a9a87d114ef10
Analyzer <analyzer@test.com>
2026-02-09 10:06:37 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,7 @@
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    ptr = 0
-    while True:
-        n = int(input[ptr])
-        m = int(input[ptr+1])
-        ptr += 2
-        if n == 0 and m == 0:
-            break
-        vectors = []
-        for _ in range(n):
-            s = input[ptr]
-            ptr += 1
-            num = int(s, 2)
-            vectors.append(num)
-        # Compute S
-        S = 0
-        for num in vectors:
-            S ^= num
-        if S == 0:
-            print(n)
-      
+y = input().strip()
+d = len(y)
+max_num = '9' * d
+if max_num >= y:
+    print(max_num)
+else:
+    print(y)
