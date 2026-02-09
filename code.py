7e75b94e19ac67a8e7cd0fa57f04e89aecafb7cd
Analyzer <analyzer@test.com>
2026-02-09 13:05:40 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,24 @@
-n, a, b, c = map(int, input().split())
-total = 0
-max_z = min(c, (2 * n) // 4)
-for z in range(max_z + 1):
-    rem = 2 * n - 4 * z
-    delta = rem - a
-    y_min = max(0, (delta + 1) // 2)
-    y_max = min(b, rem // 2)
-    if y_min > y_max:
-        continue
-    total += y_max - y_min + 1
-print(total)
+tokens = input().split()
+n = int(tokens[-1])
+lhs_tokens = tokens[:-2]
+
+coeff = [1]
+for i in range(1, len(lhs_tokens), 2):
+    op = lhs_tokens[i]
+    coeff.append(1 if op == '+' else -1)
+
+total_coeff = sum(coeff)
+T = n - total_coeff
+
+P = [i for i, c in enumerate(coeff) if c == 1]
+N = [i for i, c in enumerate(coeff) if c == -1]
+
+possible = False
+if T == 0:
+    possible = True
+elif T > 0:
+    if P and T <= len(P) * (n - 1):
+        possible = True
+else:
+    required = -T
+    if N and required <= le
