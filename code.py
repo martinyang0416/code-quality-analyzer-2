503f27fb6a812fe97ec55b4d9b55ba71dae52a7c
Analyzer <analyzer@test.com>
2026-02-09 11:26:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,17 @@
-import bisect
-
 def putaway(A, B, T, X, Y, W, S):
-    can_weak = [False] * T
-    can_small = [False] * T
-    for i in range(T):
-        w = W[i]
-        s = S[i]
-        possible = any(x > w for x in X)
-        can_weak[i] = possible
-        possible_small = any(y > s for y in Y)
-        can_small[i] = possible_small
-        if not can_weak[i] and not can_small[i]:
-            return -1
-
-    M_list = []
-    O_list = []
-    P_list = []
-    for i in range(T):
-        cw = can_weak[i]
-        cs = c
+    if T != 2:
+        return -1  # According to problem constraints, T is 2
+    
+    robots = []
+    if A == 2 and B == 0:
+        robots = [('weak', X[0]), ('weak', X[1])]
+    elif A == 1 and B == 1:
+        robots = [('weak', X[0]), ('small', Y[0])]
+    elif A == 0 and B == 2:
+        robots = [('small', Y[0]), ('small', Y[1])]
+    else:
+        return -1  # Invalid case as per problem constraints
+    
+    allowed = []
+    for toy in range(2):
+        w = W[t
