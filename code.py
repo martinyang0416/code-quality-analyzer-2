42bd0dddfd7decfa17fc0db761a19fadab23d401
Analyzer <analyzer@test.com>
2026-02-09 12:13:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,15 @@
-def fun(s):
-  if s.strip("0") == "" or s.strip("1") == "":
-    return s
- 
-  ans = ""
-  n = len(s)
- 
-  for i in range(0, n):
-    if s[0] == "0":
-        ans = ans + "01"
-    else:
-        ans = ans + "10"
-    
-  
-  return ans
- 
-tc = int(input())
- 
-while tc > 0:
-   s = str(input())
-   print(fun(s))
-   tc = tc - 1
+def binary_search(array, target):
+    lower = 0
+    upper = len(array) - 1
+    if upper == lower:
+        return 0
+    while lower < upper:   # use < instead of <=
+        x = lower + (upper - lower) // 2
+        val = array[x]
+        if target == val:
+            return x
+        elif target > val:
+            if lower == x:   # these two are the actual lines
+                dist_left = target - array[lower]
+                dist_right = array[upper] - target
+                if dist_left == dis
