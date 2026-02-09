5b62f0033ae55a2600ff4404288d1ad5ce26ec15
Analyzer <analyzer@test.com>
2026-02-09 13:37:49 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,19 @@
-def previous_permutation(arr):
-    n = len(arr)
-    k = n - 2
-    while k >= 0 and arr[k] <= arr[k + 1]:
-        k -= 1
-    if k == -1:
-        return arr
-    l = n - 1
-    while arr[l] >= arr[k]:
-        l -= 1
-    arr[k], arr[l] = arr[l], arr[k]
-    arr[k + 1:] = arr[k + 1:][::-1]
-    return arr
+MOD = 10**9 + 7
+MAX_N = 10**6
 
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    arr = list(map(int, input().split()))
-    result = previous_permutation(arr)
-    print(' '.join(map(str, result)))
+# Precompute dp array
+dp = [0] * (MAX_N + 1)
+dp[0] = 1
+if MAX_N >= 1:
+    dp[1] = 1
+
+for i in range(2, MAX_N + 1):
+    dp[i] = (dp[i-1] + (i-1) * dp[i-2]) % MOD
+
+# Read input and process test cases
+import sys
+input = sys.stdin.read().split()
+T = int(input[0])
+for i in range(1, T+1):
+    N = int(input[i])
+    print(dp[N])
