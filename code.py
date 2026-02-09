8a87d3366e3e396d448aecc9ef2afa39cb8618b9
Analyzer <analyzer@test.com>
2026-02-09 10:20:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,5 @@
-import math
-
-def compute_min_time(n, m, k, obstacles):
-    g = math.gcd(n, m)
-    d = 2 * g
-
-    results = []
-    for x, y in obstacles:
-        a_options = [x, 2 * n - x]
-        b_options = [y, 2 * m - y]
-
-        possible_ts = []
-        for a in a_options:
-            for b in b_options:
-                if (a - b) % d != 0:
-                    continue
-
-                N = 2 * n
-                M = 2 * m
-                C = (b - a) % M
-
-                g_crt = math.gcd(N, M)
-                
+k, m = map(int, input().split())
+for i in range(k):
+    start = i * m + 1
+    toys = list(range(start, start + m))
+    print(' '.join(map(str, toys)))
