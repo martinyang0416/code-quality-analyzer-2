66ed8c052e76b0b79dd4debed770d1bd292c991d
Analyzer <analyzer@test.com>
2026-02-09 14:47:44 -0800

    Update

--- code.py
+++ code.py
@@ -1,4 +1,21 @@
-def minMoves2(nums):
-    nums.sort()
-    median = nums[len(nums) // 2]
-    return sum(abs(num - median) for num in nums)
+def largestRectangleArea(heights):
+    n = len(heights)
+    if n == 0:
+        return 0
+    left = [-1] * n
+    right = [n] * n
+    stack = []
+    
+    for i in range(n):
+        while stack and heights[stack[-1]] >= heights[i]:
+            stack.pop()
+        if stack:
+            left[i] = stack[-1]
+        stack.append(i)
+    
+    stack = []
+    for i in reversed(range(n)):
+        while stack and heights[stack[-1]] >= heights[i]:
+            stack.pop()
+        if stack:
+            right[i]
