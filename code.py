37988551d42939d25104220c9edb8d61f1171413
Analyzer <analyzer@test.com>
2026-02-09 14:11:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,19 @@
-import sys
-from collections import deque
+def is_valid_square(points):
+    dists = []
+    for i in range(4):
+        for j in range(i + 1, 4):
+            dx = points[i][0] - points[j][0]
+            dy = points[i][1] - points[j][1]
+            dists.append(dx * dx + dy * dy)
+    unique = sorted(set(dists))
+    if len(unique) != 2:
+        return False
+    a, b = unique
+    if b != 2 * a or a == 0:
+        return False
+    count_a = dists.count(a)
+    count_b = dists.count(b)
+    return count_a == 4 and count_b == 2
 
-n, m, p = map(int, sys.stdin.readline().split())
-d = list(map(int, sys.stdin.readline().split()))
-
-sum_d = [0] * (n + 1)
-for i in range(2, n + 1):
-    sum_d[i] = sum_d[i-1] + d[i-2]
-
-a = []
-for _ in range(m):
-    hi, ti = map(int, sys.stdin.readline().split())
-    a.append(ti - sum_d[hi])
-
-a.sort()
-prefix = [0] * (m + 1)
-for i in range(1, m + 1):
-    prefix[i] = prefix[i-1] + a[i-1]
-
-prev_dp = [float('inf')] * (m + 1)
-prev_dp[0] = 0
-
-for _ in range(p):
- 
+n = int(input())
+fo
