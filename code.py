9b2fd373fb881a73230ff61ea2d7ae0c70385dd2
Analyzer <analyzer@test.com>
2026-02-09 10:21:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,7 @@
-MOD = 10**9 + 7
-
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    N = int(input[0])
-    V = list(map(int, input[1:N+1]))
-    
-    # Precompute factorials modulo MOD
-    fact = [1] * (N + 2)
-    for i in range(1, N + 2):
-        fact[i] = fact[i-1] * i % MOD
-    
-    # Precompute inverse of 1..N modulo MOD
-    inv = [0] * (N + 2)
-    inv[1] = 1
-    for i in range(2, N + 2):
-        inv[i] = MOD - MOD // i * inv[MOD % i] % MOD
-    
-    # Precompute harmonic numbers H[0..N+1]
-   
+L, m = map(int, input().split())
+# Discard the list of checkpoints as they are not needed
+input()
+s, t = map(int, input().split())
+distance = abs(t - s)
+min_distance = min(distance, L - distance)
+print(min_distance)
