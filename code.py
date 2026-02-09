79667a9f2d2a3219e059a8594859d6d028afbc00
Analyzer <analyzer@test.com>
2026-02-09 13:23:19 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,23 @@
-a, b, c = map(int, input().split())
-total = a + b + c
-if total % 2 != 0:
-    print("No")
-else:
-    half = total // 2
-    if (a == half or b == half or c == half) or (a + b == half) or (a + c == half) or (b + c == half):
-        print("Yes")
-    else:
-        print("No")
+import heapq
+
+class Edge:
+    def __init__(self, to, rev, capacity, cost):
+        self.to = to
+        self.rev = rev
+        self.capacity = capacity
+        self.cost = cost
+
+def add_edge(adj, fr, to, capacity, cost):
+    adj[fr].append(Edge(to, len(adj[to]), capacity, cost))
+    adj[to].append(Edge(fr, len(adj[fr])-1, 0, -cost))
+
+def min_cost_flow(adj, s, t, maxf):
+    n = len(adj)
+    res = 0.0
+    h = [0.0] * n
+    prevv = [0] * n
+    preve = [0] * n
+    INF = float('inf')
+
+    flow = 0
+  
