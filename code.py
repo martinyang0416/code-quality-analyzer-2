28e7085794e7ca5d541964b943425a6a4035a410
Analyzer <analyzer@test.com>
2026-02-09 11:26:28 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,25 @@
 def putaway(A, B, T, X, Y, W, S):
-    if T != 2:
-        return -1  # According to problem constraints, T is 2
-    
-    robots = []
-    if A == 2 and B == 0:
-        robots = [('weak', X[0]), ('weak', X[1])]
-    elif A == 1 and B == 1:
-        robots = [('weak', X[0]), ('small', Y[0])]
-    elif A == 0 and B == 2:
-        robots = [('small', Y[0]), ('small', Y[1])]
-    else:
-        return -1  # Invalid case as per problem constraints
-    
-    allowed = []
-    for toy in range(2):
-        w = W[t
+    max_X = -float('inf')
+    if A > 0:
+        max_X = max(X)
+    max_Y = -float('inf')
+    if B > 0:
+        max_Y = max(Y)
+
+    W_only = 0
+    S_only = 0
+    Both = 0
+
+    for i in range(T):
+        w = W[i]
+        s = S[i]
+        weak_ok = (w < max_X)
+        small_ok = (s < max_Y)
+        if weak_ok and small_ok:
+            Both += 1
+        elif weak_ok:
+            W_only += 1
+        elif small_ok:
+            S_only += 1
+        else:
+            ret
