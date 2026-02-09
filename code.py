fbabcd11401d4d1c654c025b7f6888d28777c282
Analyzer <analyzer@test.com>
2026-02-09 14:50:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,14 @@
-def findKthNumber(lo, hi, k):
-    def get_power(x, memo={1: 0}):
-        if x not in memo:
-            if x % 2 == 0:
-                next_x = x // 2
-            else:
-                next_x = 3 * x + 1
-            memo[x] = 1 + get_power(next_x, memo)
-        return memo[x]
-    
-    numbers = list(range(lo, hi + 1))
-    power_list = [(get_power(num), num) for num in numbers]
-    power_list.sort(key=lambda x: (x[0], x[1]))
-    return power_list[k-1][1]
+def minSwap(A, B):
+    n = len(A)
+    swap, noswap = 1, 0
+    for i in range(1, n):
+        new_swap = float('inf')
+        new_noswap = float('inf')
+        # Transition from previous not swapped
+        if A[i] > A[i-1] and B[i] > B[i-1]:
+            new_noswap = min(new_noswap, noswap)
+        if B[i] > A[i-1] and A[i] > B[i-1]:
+            new_swap = min(new_swap, noswap + 1)
+        # Transition from previous swapped
+        if A[i] > B[i-1] and B[i] > A[i-1]:
+            new_noswap = min(n
