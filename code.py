0b1e602e85a74cffaf7318e644f4168e7701eb80
Analyzer <analyzer@test.com>
2026-02-09 12:16:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,33 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+from math import ceil, log, floor, sqrt
+import math	
+	
+	
+k = 1	
+def mod_expo(n, p, m):
+	"""find (n^p)%m"""
+	result = 1
+	while p != 0:
+		if p%2 == 1:
+			result = (result * n)%m
+		p //= 2
+		n = (n * n)%m
+	return result
+	
+def is_prime(n):
+	m = 2
+	while m*m <= n:
+		if n%m == 0:
+			return False
+		m += 1
+	return True
+	
+def find_sum(n, a):
+	a.insert(0, 0)
+	for i in range(1, n+1):
+		prev = a[i] & a[i-1]
+		cur = a[i] | a[i-1]
+		a[i-1] = prev
+		a[i] = cur
+	return sum(m*m for m in a)
 
-import builtins
-import re
+def prin_abc(x, y, z)
