a36f3426d704b547adb9e39ced21bea55c3544c4
Analyzer <analyzer@test.com>
2026-02-09 10:19:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,23 @@
-def find_dominant_tree(t, test_cases):
+import math
+
+def compute_min_time(n, m, k, obstacles):
+    g = math.gcd(n, m)
+    d = 2 * g
+
     results = []
-    for case in test_cases:
-        n, h = case
-        if n == 1:
-            results.append(1)
-            continue
-        
-        left = [0] * n
-        for i in range(1, n):
-            if h[i-1] < h[i]:
-                left[i] = left[i-1] + 1
-            else:
-                left[i] = 0
-        
-        right = [0] * n
-        for i in range(n-2, -1, -1):
-            if h[i] < h[i+1]:
-                right[i] = right[i+1] + 1
-      
+    for x, y in obstacles:
+        a_options = [x, 2 * n - x]
+        b_options = [y, 2 * m - y]
+
+        possible_ts = []
+        for a in a_options:
+            for b in b_options:
+                if (a - b) % d != 0:
+                    continue
+
+                N = 2 * n
+                M = 2 * m
+                C = (b - a) % M
+
+                g_crt = math.gcd(N, M)
+                
