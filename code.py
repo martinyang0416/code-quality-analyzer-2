d05c025fbffd798f21a359f57b7254571cf1d61d
Analyzer <analyzer@test.com>
2026-02-09 14:10:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,25 @@
 import sys
+from collections import deque
 
-def main():
-    n = int(sys.stdin.readline())
-    dp = [[-float('inf')] * 26 for _ in range(26)]
-    
-    for _ in range(n):
-        name = sys.stdin.readline().strip()
-        if not name:
-            continue  # handle empty lines if any
-        s = ord(name[0]) - ord('a')
-        e = ord(name[-1]) - ord('a')
-        l = len(name)
-        
-        new_trans = {}
-        
-        # Standalone case
-        key = (s, e)
-        if key in new_trans:
-            if l > new_trans[key]:
+n, m, p = map(int, sys.stdin.readline().split())
+d = list(map(int, sys.stdin.readline().split()))
+
+sum_d = [0] * (n + 1)
+for i in range(2, n + 1):
+    sum_d[i] = sum_d[i-1] + d[i-2]
+
+a = []
+for _ in range(m):
+    hi, ti = map(int, sys.stdin.readline().split())
+    a.append(ti - sum_d[hi])
+
+a.sort()
+prefix = [0] * (m + 1)
+for i in range(1, m + 1):
+    prefix[i] = prefix[i-1] + a[i-1]
+
+prev_dp = [float('inf')] * (m + 1)
+prev_dp[0] = 0
+
+for _ in range(p):
  
