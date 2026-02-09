5c4399c76493b713d15549ecb0a2540f9a13de78
Analyzer <analyzer@test.com>
2026-02-09 10:27:33 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,27 @@
-import sys
+from collections import deque
 
 def main():
-    while True:
-        n, m = map(int, sys.stdin.readline().split())
-        if n == 0 and m == 0:
-            break
-        degree = [0] * n
-        for _ in range(m):
-            x, y, c = map(int, sys.stdin.readline().split())
-            degree[x] += c
-            degree[y] += c
-        min_cut = min(degree)
-        print(min_cut)
+    import sys
+    input = sys.stdin.read().split()
+    idx = 0
+    N = int(input[idx])
+    idx += 1
+    M = int(input[idx])
+    idx += 1
 
-if __name__ == "__main__":
-    main()
+    adj = [[] for _ in range(N)]
+    for _ in range(M):
+        u = int(input[idx])
+        idx += 1
+        v = int(input[idx])
+        idx += 1
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    levels = [-1] * N
+    levels[0] = 0
+    q = deque([0])
+
+    while q:
+        u = q.popleft()
+        for v in adj
