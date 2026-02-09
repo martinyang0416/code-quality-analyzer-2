1a68de318c56ce3113911166081fd4d5898bb824
Analyzer <analyzer@test.com>
2026-02-09 14:09:15 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,19 @@
-n = int(input())
-a = list(map(int, input().split()))
-t = list(map(int, input().split()))
+import sys
 
-categories = sorted(zip(t, a), key=lambda x: (-x[0], x[1]))
-parent = {}
-
-def find(x):
-    if x not in parent:
-        return x
-    root = x
-    while root in parent:
-        root = parent[root]
-    current = x
-    while current in parent and parent[current] != root:
-        next_node = parent[current]
-        parent[current] = root
-        current = next_node
-    return root
-
-total = 0
-for ti, ai in categor
+class DSU:
+    def __init__(self, size):
+        self.parent = list(range(size+1))
+        self.rank = [0]*(size+1)
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        if x_root == y_root:
+            return False
+        if self.rank[x_root] < self.rank[y_root]:
+            self.parent[x_root]
