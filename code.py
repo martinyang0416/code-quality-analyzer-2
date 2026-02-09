21eb8d81d4408c82dbaa975c51cd0f3de4056ac3
Analyzer <analyzer@test.com>
2026-02-09 13:41:40 -0800

    Update

--- code.py
+++ code.py
@@ -1,26 +1,22 @@
-import math
+n, m = map(int, input().split())
+INF = float('-inf')
+adj = [[INF] * n for _ in range(n)]
 
-x = int(input())
+for _ in range(m):
+    i, j, c_ij, c_ji = map(int, input().split())
+    i -= 1
+    j -= 1
+    adj[i][j] = c_ij
+    adj[j][i] = c_ji
 
-if x == 1:
-    print(1, 1)
-else:
-    def factorize(n):
-        factors = {}
-        while n % 2 == 0:
-            factors[2] = factors.get(2, 0) + 1
-            n = n // 2
-        i = 3
-        while i * i <= n:
-            while n % i == 0:
-                factors[i] = factors.get(i, 0) + 1
-                n = n // i
-            i += 2
-        if n > 1:
-            factors[n] = 1
-        return factors
+# Check for 2-length cycles
+for i in range(n):
+    for j in range(n):
+        if i != j and adj[i][j] != INF and adj[j][i] != INF:
+            if adj[i][j] + adj[j][i] > 0:
+                print(2)
+                exit()
 
-    factors = factorize(x)
-    
-    divisors = [1]
-    for p 
+prev = [row[:] for row in adj]
+
+for k in range(2, n + 1
