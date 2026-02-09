4d8545d4191b1f8e9d6a7287218815a096d195b7
Analyzer <analyzer@test.com>
2026-02-09 13:51:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,23 @@
-import bisect
+MOD = 10**9 + 7
 
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    idx = 0
-    t = int(data[idx])
-    idx += 1
-    for _ in range(t):
-        n, m = int(data[idx]), int(data[idx+1])
-        idx +=2
-        a = list(map(int, data[idx:idx+n]))
-        idx +=n
-        b = list(map(int, data[idx:idx+n]))
-        idx +=n
-        
-        type1 = []
-        type2 = []
-        for ai, bi in zip(a, b):
-            if bi == 1:
-                type1.append(ai)
-            
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+if k == 0:
+    print(' '.join(map(str, a)))
+    exit()
+
+max_fact = 2000
+fact = [1] * (max_fact + 1)
+for i in range(1, max_fact + 1):
+    fact[i] = fact[i-1] * i % MOD
+
+inv_fact = [1] * (max_fact + 1)
+inv_fact[max_fact] = pow(fact[max_fact], MOD - 2, MOD)
+for i in range(max_fact - 1, -1, -1):
+    inv_fact[i] = inv_fact[i+1] * (i+1) % MOD
+
+comb = [0] * (2001)
+for d in range(2001):
+    if d == 0:
+        comb[d] 
