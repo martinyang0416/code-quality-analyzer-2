7a8adfea16fd35351b4c540d8f992885783db979
Analyzer <analyzer@test.com>
2026-02-09 13:34:16 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,16 @@
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    if n == 1:
-        print(0)
-        continue
-    a = 0
-    while n % 2 == 0:
-        a += 1
-        n = n // 2
-    b = 0
-    while n % 3 == 0:
-        b += 1
-        n = n // 3
-    if n != 1:
-        print(-1)
-    else:
-        if a > b:
-            print(-1)
+n = int(input())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+elif n == 3:
+    print(6)
+else:
+    if n % 2 == 0:
+        if n % 3 == 0:
+            print((n-1)*(n-2)*(n-3))
         else:
-            print(2 * b - a)
+            print(n*(n-1)*(n-3))
+    else:
+        print(n*(n-1)*(n-2))
