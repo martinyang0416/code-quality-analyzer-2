6bf3b956923ecd53d7c18a296ac776b00c41c9b8
Analyzer <analyzer@test.com>
2026-02-09 13:25:02 -0800

    Update

--- code.py
+++ code.py
@@ -1,4 +1,20 @@
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    print((N // 2) + 1)
+import sys
+from collections import deque
+
+def main():
+    sys.setrecursionlimit(1 << 25)
+    n, k = map(int, sys.stdin.readline().split())
+    parents = list(map(int, sys.stdin.readline().split()))
+    children = [[] for _ in range(n+1)]  # 1-based
+    for i in range(2, n+1):
+        p = parents[i-2]
+        children[p].append(i)
+    
+    # Compute depths using BFS
+    depth = [0] * (n + 1)
+    q = deque()
+    q.append(1)
+    while q:
+        u = q.popleft()
+        for v in children[u]:
+       
