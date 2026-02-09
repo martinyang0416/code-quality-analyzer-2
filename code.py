f03626a53a6faab4598bd31923fd2eb80d5a8d8b
Analyzer <analyzer@test.com>
2026-02-09 13:31:49 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,9 @@
-import sys
-
-def main():
-    while True:
-        line = sys.stdin.readline()
-        if not line:
-            break
-        N, M, L = map(int, line.strip().split())
-        if N == 0 and M == 0 and L == 0:
-            break
-        data = list(map(float, sys.stdin.readline().strip().split()))
-        
-        # Precompute error for all intervals [start][end] (0-based, inclusive)
-        error = [[0.0 for _ in range(N)] for _ in range(N)]
-        for start in range(N):
-            current_sub = []
+n = int(input())
+a = list(map(int, input().split()))
+q = int(input())
+for _ in range(q):
+    b, e, t = map(int, input().split())
+    length = e - b
+    for k in range(length):
+        a[b + k], a[t + k] = a[t + k], a[b + k]
+print(' '.join(map(str, a)))
