10415b9c0011f0320dba14b37091f01bf1ae3515
Analyzer <analyzer@test.com>
2026-02-09 15:18:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,25 @@
-n = int(input())
-health = list(map(int, input().split()))
-P = int(input())
+n, k, s = map(int, input().split())
+a = list(map(int, input().split()))
 
-health_sorted = sorted(health)
-sum_initial = 0
-m = 0
-for h in health_sorted:
-    if sum_initial + h > P:
-        break
-    sum_initial += h
-    m += 1
-remaining_energy = P - sum_initial
-remaining_candidates = health_sorted[m:]
-max_total = 0
+prefix = [0] * (n + 1)
+for i in range(n):
+    prefix[i+1] = prefix[i] + a[i]
 
-max_k = min(m, len(remaining_candidates))
-for k in range(0, max_k + 1):
-    if k > len(remaining_candidates):
-        continue
-    sum_action2 = sum(remaining_candidates[-k:]) if k >
+left = k + 1
+right = n
+ans = -1
+
+while left <= right:
+    mid = (left + right) // 2
+    if mid > n:
+        valid = False
+    else:
+        valid = any((prefix[i + mid] - prefix[i] > s) for i in range(n - mid + 1))
+    
+    if valid:
+        ans = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(ans if ans != -1 else -1)
