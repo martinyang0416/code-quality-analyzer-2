04486661025eb582a1f243800c5267adaece3d7d
Analyzer <analyzer@test.com>
2026-02-09 13:53:51 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,9 @@
-k, n = map(int, input().split())
-stones = [tuple(map(int, input().split())) for _ in range(k)]
-monsters = [tuple(map(int, input().split())) for _ in range(n)]
-
-count = 0
-
-for m in monsters:
-    mx, my = m
-    afraid = False
-    for t in stones:
-        tx, ty = t
-        dx = mx - tx
-        dy = my - ty
-        distance_sq_m = dx ** 2 + dy ** 2
-        blocked = False
-        for m_prime in monsters:
-            if m_prime == m:
-                continue
-            mpx, mpy = m_prime
-          
+s, v1, v2, t1, t2 = map(int, input().split())
+time1 = 2 * t1 + s * v1
+time2 = 2 * t2 + s * v2
+if time1 < time2:
+    print("First")
+elif time2 < time1:
+    print("Second")
+else:
+    print("Friendship")
